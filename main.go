aa082b19baafe18a85da53ed6b35897d01537df4
Raul C Siqueira <95100546+RaulCSiqueira@users.noreply.github.com>
2022-06-10 01:06:43 -0300

    Add files via upload

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func main() {
+	conferenceName := "Go Conference"
+	const conferenceTickets int = 50
+	var remainingTickets uint = 50
+	bookings := []string{}
+
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to ettend")
+
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
+		// ask user for their name
+		fmt.Println("Enter your first name:")
+		fmt.Scan(&firstName)
+		fmt.Println("Enter your last name:")
+		fmt.Scan(&lastName)
+		fmt.Println("Enter your E-mail:")
+		fmt.Scan(&email)
+		fmt.Println("Enter number of tickets")
+		fmt.Scan(&userTickets)
+
+		remainingTickets = remainingTickets - userTickets
+		bookings = append(bookings, firstName+" "+lastName)
+
+		fmt.Printf("Tahank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+		fmt.Printf("%v tickets remaining for %v \n", remainingTickets, conferenceTickets)
+		
+		firstName := []string{}
+		for index, booking := range bookings
+		fmt.Printf("These are all our booking: %v\n", bookings)
+	}
+}
